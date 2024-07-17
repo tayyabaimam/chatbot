b2a32c67e88ae1c0bf48ef903eb7470de47615d3
Tayyaba Imam <tayyaba.imam@gmail.com>
2024-07-17 21:58:39 +0500

    creating chatbot with chatclient

---
+++ src/main/java/com/tayyaba/RAG/Chatbot/RagChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.tayyaba.RAG.Chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tayyaba/RAG/Chatbot/RagChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tayyaba.RAG.Chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
